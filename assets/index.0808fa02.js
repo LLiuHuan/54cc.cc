a28a8e15308841b6f5587267a8262e3bdcc01703
刘欢 <184555556@qq.com>
2022-04-21 21:59:47 +0800

    Updates

---
+++ assets/index.0808fa02.js
@@ -0,0 +1,1 @@
+import{u as s,b as n}from"./vue-router.48670fbe.js";import{d as u,A as m,v as p}from"./@vue.f65743c1.js";import{E as c}from"./@arco-design.1a330ff8.js";var l=u({name:"Redirect",setup(){const r=s(),t=n();return m(()=>{const{params:e,query:a}=r,{path:o}=e;console.log(r,e),t.replace({path:"/"+(Array.isArray(o)?o.join("/"):o),query:a})}),()=>p(c)}});export{l as default};
---
+++ assets/index.4f14b41d.js
@@ -0,0 +1,1 @@
+import{d as e,t as n}from"./@vue.f65743c1.js";const p=e({setup(o){const t="Layout";return(a,r)=>n(t)}});export{p as default};
